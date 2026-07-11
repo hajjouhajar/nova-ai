0a370d7e2ba07563c8fcff757d8e02d367f8c5fb
Wiam Benkrimen <bbenkrimen@gmail.com>
2026-07-11 17:40:43 +0200

    feat(projects): add Project and Task viewsets

--- backend/apps/projects/views.py
+++ backend/apps/projects/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Project, Task
+from .serializers import ProjectSerializer, TaskSerializer
 
-# Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(project__user=self.request.user)
